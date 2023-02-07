41a9c7f4fbc605d21a0ec5770256a5f1834570be
subzero <shubham8111994@gmail.com>
2023-02-08 01:57:00 +0530

    added function for toss to check who plays first

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 //Program to simulate TicTacToe game between computer and player
 public class TicTacToeGame {
-    public static char letter,player,computer;
+    public static char letter,playerValue,computerValue;
     public static char[] gameBoard = new char[10];
     public static int chance;
+    public static int boardFull = 1;
+
+    public static int toss(){
+    return (int)(Math.random() * 2);
+    }
     public static char chooseLetterXorO(){  //Method to give player chance to choose character X or O before starting game
         System.out.println("Choose a letter between x or o before starting game");
         Scanner scanner = new Scanner(System.in);
@@ -30,7 +35,7 @@ public class TicTacToeGame {
         move = scanner.nextInt();
         while (success == false){                           //Empty index check
             if (gameBoard[move] == '-') {
-                gameBoard[move] = player;
+                gameBoard[move] = playerValue;
                 success = true;
             } else {
                 System.out.println("its already taken, kindly try another index");
@@ -38,6 +43,26 @@ public class TicTacToeGame {
             }
     }
         showBoard();
+        boardFull++;
+        chance=1;           //Computer chance
+    }
+
+    public static void computerMove(){
+
+       int computerMove = (int)(Math.random() * 9 + 1);
+        boolean success = false;
+        while (success == false){                           //Empty index check
+            if (gameBoard[computerMove] == '-') {
+                gameBoard[computerMove] = computerValue;
+                success = true;
+            } else {
+                computerMove = (int)(Math.random() * 9 + 1);
+
+            }
+        }
+        showBoard();
+        boardFull++;
+        chance=2;       //Player Chance
     }
     public static void main(String[] args) {
 
@@ -47,16 +72,30 @@ public class TicTacToeGame {
         for (int i = 1; i < 10; i++) {
             gameBoard[i] = '-';
         }
-        player=chooseLetterXorO();
-        if (player=='x'){
-            computer = 'o';
+        playerValue=chooseLetterXorO();
+        if (playerValue=='x'){
+            computerValue = 'o';
         }else {
-            computer = 'x';
+            computerValue = 'x';
         }
         showBoard();
 
-        playerMove();
 
+            if (toss() == 1) {                                 // If 1 then user win and plays first
+                System.out.println("Player turn");
+                playerMove();
+            } else {
+                System.out.println("Computer turn");
+                computerMove();
+            }
 
+            while (boardFull<10){
+                if(chance==1){
+                    System.out.println("Computer turn");
+                    computerMove();
+                }else{
+                    playerMove();
+                }
+            }
     }
 }
